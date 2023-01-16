7790e7b14a51f1f7bd756667516c2d02a1ae9666
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2023-01-15 19:06:20 -0500

    2nd method to update time and date

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 "use strict";
 const clockEl = document.getElementById('clock');
+let x = 0;
+// Method 1
 
-const displayHour = function () {
+const displayDate = function () {
     let date = new Date();
     clockEl.textContent = date;
+    console.log(x++);
 }
-displayHour();
+//setInterval call displayDate every second.
+setInterval(displayDate, 1000);
+
+// Method 2
+function display_c() {
+    // var refresh = 1000; // Refresh rate in milli seconds
+    setTimeout('display_ct()', 1000)
+}
+
+function display_ct() {
+    var x = new Date()
+    document.getElementById('clock2').innerHTML = x;
+    display_c();
+}
+// display_ct display Date and call display_c. display_c waite 1s and call again display_ct 
+display_ct();
+
 
